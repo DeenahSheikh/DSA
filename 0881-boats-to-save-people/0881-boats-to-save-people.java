d26ee7010c243f3461d6d8f01a282947171a258b
Deenah Sheikh <deenahsheikh20@gmail.com>
2026-07-12 23:26:52 +0530

    Time: 21 ms (24.46%), Space: 57.4 MB (5.46%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+       Arrays.sort(people);
+        int left=0;
+        int right=people.length-1;
+
+        int boats=0;
+        while(right>=left){
+            if(people[right]+people[left]<=limit){
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+}
