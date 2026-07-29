3a843f585960e7d58f43fcf7ed6ebbad790691fd
Deenah Sheikh <deenahsheikh20@gmail.com>
2026-07-29 09:46:59 +0530

    Time: 17 ms (16.63%), Space: 46.7 MB (16.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,12 +2,25 @@ import java.util.*;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char ch1[]=s.toCharArray();
-        char ch2[]=t.toCharArray();
+        if(s.length()!=t.length()){
+            return false;
+        }
 
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
+        HashMap<Character,Integer> map=new HashMap<>();
 
-        return Arrays.equals(ch1,ch2);
-    }
-}
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+        map.put(ch,map.get(ch)-1);
+
+        if(map.get(ch)==0){
+            map.remove(ch);
+        }
+        }
+           
+    return map.isEmpty();
+}}
