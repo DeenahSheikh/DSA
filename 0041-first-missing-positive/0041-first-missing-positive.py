5f3f9bb9f67006fb7ed9303c2eb6afd297edc5f7
Deenah Sheikh <deenahsheikh20@gmail.com>
2026-08-11 22:32:29 +0530

    Time: 18 ms (89.71%), Space: 35.2 MB (7.29%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        n = 1
+        while True:
+            if n not in nums:
+                return n
+            n+=1
